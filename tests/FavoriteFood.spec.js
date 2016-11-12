c7412067606de07859ae03bdcb5465b803b99302
arturusfury <bri.reynolds@gmail.com>
2016-11-12 17:37:14 -0600

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -1,11 +1,31 @@
 describe('Favorite Food Filter', function () {
-	var $controller;
+	var $controller, favoriteFood;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+  beforeEach(inject(function ($filter) {
+		favoriteFood = $filter('favoriteFood');
 	}));
 
-	
+  it('should filter an array of objects based on input', function () {
+    var madMaxPeople = [
+      {
+        name: "Max",
+        favoriteFood: 'Octane'
+      },
+      {
+        name: "Furiosa",
+        favoriteFood: 'Seeds'
+      },
+      {
+        name: "Nux",
+        favoriteFood: 'Octane'
+      }
+    ];
+
+    var results = favoriteFood(madMaxPeople, 'Seeds');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Furiosa');
+  });
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -1,10 +1,13 @@
 describe('removeAllVowels Filter', function () {
-	var $controller;
+	var $controller, removeAllVowels;
 
 	beforeEach(module('app'));
 
-	beforeEach(inject(function ($injector) {
-		$filter = $injector.get('$filter');
+	beforeEach(inject(function ($filter) {
+		removeAllVowels = $filter('removeAllVowels');
 	}));
 
+  it('should remove all vowels from a string', function () {
+    expect(removeAllVowels('test')).toEqual('tst');
+  });
 });
